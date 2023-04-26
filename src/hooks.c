b7ddb9f33fdba0037733d267362c446bc8a3850f
Francis Rafal <francis.rafal@gmail.com>
2023-04-26 13:42:48 +0200

    fix: Remove render_background in loop_hook

--- src/hooks.c
+++ src/hooks.c
@@ -4,7 +4,6 @@ int	loop_hook(t_data *data)
 {
 	if (data->win_ptr == NULL)
 		return (1);
-	render_background(data, BLACK);
 	render_scene(data);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
 		data->img->mlx_img, 0, 0);
