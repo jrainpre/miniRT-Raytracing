e33cc9b3bb5f78bffa0bbdfe3cca827666bd4c25
Francis Rafal <francis.rafal@gmail.com>
2023-05-16 23:44:03 +0200

    fix: Norm ft_atof

--- src/ft_atof.c
+++ src/ft_atof.c
@@ -6,11 +6,24 @@
 /*   By: frafal <frafal@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 23:13:51 by frafal            #+#    #+#             */
-/*   Updated: 2023/05/16 23:13:52 by frafal           ###   ########.fr       */
+/*   Updated: 2023/05/16 23:41:51 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+static int	get_decimal_digits(const char *str, int i)
+{
+	int	decimal_digits;
+
+	decimal_digits = 0;
+	while (ft_isdigit(str[i]) && str[i] != '\0')
+	{
+		decimal_digits++;
+		i++;
+	}
+	return (decimal_digits);
+}
 
 double	ft_atof(const char *str)
 {
@@ -30,16 +43,10 @@ double	ft_atof(const char *str)
 	i = 0;
 	while (str[i] != '.' && str[i] != '\0')
 		i++;
-	if (str[i] == '\0')
+	if (str[i++] == '\0')
 		return (num);
-	i++;
 	decimal = ft_atoi(str + i);
-	decimal_digits = 0;
-	while (ft_isdigit(str[i]) && str[i] != '\0')
-	{
-		decimal_digits++;
-		i++;
-	}
+	decimal_digits = get_decimal_digits(str, i);
 	num = num + sign * (decimal / pow(10, decimal_digits));
 	return (num);
 }
