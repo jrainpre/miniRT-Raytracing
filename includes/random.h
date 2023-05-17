7dbabd138d7dbccf7644c100e7fa6cbfcff4bd2d
Francis Rafal <francis.rafal@gmail.com>
2023-05-17 10:37:11 +0200

    fix: Cylinder And Default Scene

--- includes/random.h
+++ includes/random.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   random.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frafal <frafal@student.42vienna.com>       +#+  +:+       +#+        */
+/*   By: frafal <frafal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 23:45:36 by frafal            #+#    #+#             */
-/*   Updated: 2023/05/16 23:49:14 by frafal           ###   ########.fr       */
+/*   Updated: 2023/05/17 10:33:44 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # define FUZZ_FACTOR 0.0000001
 # define VEC_OFFSET 0.0001
-# define SHADOW_OFFSET 0.0001
+# define SHADOW_OFFSET 0.001
 # define SHADOW_SOFT_MULTIPLIER 0
 # include <time.h>
 
